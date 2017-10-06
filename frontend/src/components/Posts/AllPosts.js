56fad66d05fd55529168a356e70537dde7b494c4
fernmun <lfmparra@gmail.com>
2017-10-06 11:06:01 -0500

    [feat] Add Vote component in AllPosts view

--- frontend/src/components/Posts/AllPosts.js
+++ frontend/src/components/Posts/AllPosts.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getAllPosts } from '../../actions'
-import ListPosts from './ListPosts'
+import { Link } from 'react-router-dom'
+import Vote from '../Vote/Vote'
+import { getAllPosts, votePost } from '../../actions'
 
 class AllPosts extends Component {
   componentDidMount() {
     this.props.getAllPosts(this.props.match.params.category)
   }
 
+  renderPost(post) {
+    return (
+      <div key={post.id} className="post">
+        <Vote handleVote={this.props.votePost} id={post.id} score={post.voteScore} />
+        <h1><Link className="title" to={`/${post.category}/${post.id}`}>{post.title}</Link></h1>
+        <h6><Link to={`/${post.category}/${post.id}/edit`}>Edit</Link></h6>
+        <h6><Link to={`/${post.category}`}>{post.category}</Link></h6>
+        <p>{post.body}</p>
+        <span>{post.id}</span>
+      </div>
+    )
+  }
+
   render() {
+    if (this.props.posts.length === 0)
+      return (
+        `There are no posts to be shown! :(`
+      )
+
     return(
-      <ListPosts posts={this.props.posts} />
+      <div>
+        {this.props.posts.map(post => this.renderPost(post))}
+      </div>
     )
   }
 }
@@ -24,6 +45,7 @@ function mapStateToProps (state) {
 function mapDispatchToProps (dispatch) {
   return {
     getAllPosts: (category) => dispatch(getAllPosts(category)),
+    votePost: (id, vote) => dispatch(votePost(id, vote))
   }
 }
 
--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,5 +1,6 @@
 import {
   ADD_POST,
+  VOTE_POST,
   GET_ALL_POSTS
 } from '../const/actions'
 
@@ -13,8 +14,14 @@ function posts (state = [], action) {
           comments: []
         }
       }
+    case VOTE_POST:
+      const key = state.findIndex(post => action.payload.id === post.id)
+      let posts = Object.values(state)
+      posts[key] = action.payload
+
+      return posts
     case GET_ALL_POSTS:
-      return action.payload
+      return Object.values(action.payload)
     default:
       return state
   }
